359cbdfc9785a6032cc4c3d0ab335f520a389a2e
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-11-20 21:16:26 +0100

    refactor cookie consent

--- Frontend-project/11-cookie-consent/scripts/script.js
+++ Frontend-project/11-cookie-consent/scripts/script.js
@@ -1,23 +1,22 @@
+const popup = document.getElementById("cookie-popup");
+
+document
+  .getElementById("accept-cookies")
+  .addEventListener("click", acceptCookies);
+
+if (!checkCookieConsent()) {
+  showCookiePopup();
+}
+
+function acceptCookies() {
+  popup.style.display = "none";
+  localStorage.setItem("cookieConsent", "true");
+}
+
 function checkCookieConsent() {
-    return localStorage.getItem("cookieConsent") === "true";
-  }
-  
-  function showCookiePopup() {
-    const popup = document.getElementById("cookie-popup");
-    popup.style.display = "flex";
-  }
-   
-  function acceptCookies() {
-    localStorage.setItem("cookieConsent", "true");
-    const popup = document.getElementById("cookie-popup");
-    popup.style.display = "none";
-  }
-  
-  document
-    .getElementById("accept-cookies")
-    .addEventListener("click", acceptCookies);
-  
-  if (!checkCookieConsent()) {
-    showCookiePopup();
-  }
-  
+  return localStorage.getItem("cookieConsent") === "true";
+}
+
+function showCookiePopup() {
+  popup.style.display = "block";
+}
