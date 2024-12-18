fe304164520b77ef5e3f95a86e8f91697852d421
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-12-18 22:14:29 +0100

    steps for odd numbers sequence

---
+++ class-examples/odd_numbers.js
@@ -0,0 +1,9 @@
+// Problem: generate x number of odd numbers
+// 1 3 5 7 9 11 13 15 17 19
+
+// Solution steps;
+// counting through numbers
+// check if each number is an odd number
+// a number is odd if it has a remainder after it is divided by 2
+// if the number is odd, add it to the group of numbers to be returned
+// by default, the first odd number is 1
