16b9a8606e553936a7974e49eb2133f55150b63d
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-12-23 23:02:39 +0100

    user class assigment

---
+++ class-examples/user.js
@@ -0,0 +1,25 @@
+/**
+ * Create a User class
+ * 
+ * the user will have the following properties
+ * - fullName
+ * - emailAddress
+ * - phoneNumber
+ * - password
+ * - dateOfBirth
+ * - gender
+ * - country
+ * - profession
+ * 
+ * the user object should be able to do the following
+ * - validate the following;
+ * - - emailAddress - check if it is a valid email address
+ * - - phoneNumber - valid phone number (should be 11 digits)
+ * - - password - at least 8 characters in length
+ * - - dateOfBirth - valid date format and user's age should be at least 18
+ * - - gender - valid gender (between male and female)
+ * 
+ * login - login using email and password
+ * updatePassword - validate the current password before updating to the newly provide password
+ * 
+ */
