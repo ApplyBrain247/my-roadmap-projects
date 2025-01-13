fe3e8ccd930dcc5abae89a5288cce66c2c0404ba
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2025-01-13 21:27:30 +0100

    make profile elements dynamic with JS

---
+++ class-examples/netflix-account-page/scripts/script.js
@@ -0,0 +1,25 @@
+import Profile from './profile.js'
+
+// create an array of the profiles
+const profiles = [
+  new Profile('ACE', 'images/netflix-img.png', true),
+  new Profile('Apply', 'https://avatars.githubusercontent.com/u/170747869?v=4', true),
+  new Profile('Faith', 'images/barbie.jpg', true),
+  new Profile('FAM', 'images/netflix-profile-pictures.webp'),
+  new Profile('Barbie', 'images/barbie.jpg', true),
+  new Profile('Kids', 'images/kids-logo.png')
+]
+
+// loop through the profiles and create the html for each one
+// append the html to the parent element
+let profileHtml = ''
+for (let profile of profiles) {
+  profileHtml += `
+    <div class="img-container">
+      <img src="${profile.imagePath}" alt="#" width="100px" height="100px">
+      <p>${profile.name}</p>
+    </div>
+  `
+}
+
+document.querySelector('.main-container').insertAdjacentHTML('afterbegin', profileHtml)
