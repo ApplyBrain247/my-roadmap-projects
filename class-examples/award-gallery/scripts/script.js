12b177e4f3e337d3ad1879d87d81e6182b93b71d
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2025-02-10 21:06:07 +0100

    make award categories dynamic

--- class-examples/award-gallery/scripts/script.js
+++ class-examples/award-gallery/scripts/script.js
@@ -1,39 +1,46 @@
-import Category from './category'
-import Project from './project'
-import Artiste from './artiste'
+import Category from './category.js'
+import Project from './project.js'
+import Artiste from './artiste.js'
 
 const kendrick = new Artiste ('Kendrick lamar')
 const beyonce = new Artiste ('Beyonce Knowles')
 const billie = new Artiste ('Billie Eillish')
+const doechii = new Artiste('Doechii')
 
-const nonLikeUs = new Project ('Not like Us', kendrick ,'kendrick-lamar.jpeg')
-const birdsOfAFeather = new Project ('Birds Of A Feather', billie, 'billie-eillish.webp')
-const texas = new Project ('Texas Hold Em', beyonce, 'beyonce.webp')
+const nonLikeUs = new Project ('Not like Us', kendrick ,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKEvkLFCXXu6M-wQzar-CphRcNkXE62Rao4Q&s')
+const birdsOfAFeather = new Project ('Birds Of A Feather', billie, 'https://upload.wikimedia.org/wikipedia/en/f/fe/Billie_Eilish_-_Birds_of_a_Feather_7%22_Vinyl_cover.png')
+const texas = new Project ('Texas Hold Em', beyonce, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdg_XydwQGsO-J77cxwT9ZdOTbbJt5KvZHNQ&s')
+const ABNH = new Project('Alligator Bites Never Heal', doechii, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiwh1nR7Xwh2hmlVEPTifvstmQ-UcXVnYpsg&s')
 
 const songOfTheYear = new Category ('Song Of The year', [nonLikeUs, birdsOfAFeather, texas])
-const recordOfTheYear = new Category ('Song Of The Year', [nonLikeUs, birdsOfAFeather, texas])
+const recordOfTheYear = new Category ('Record Of The Year', [nonLikeUs, birdsOfAFeather, texas])
+const rapAlbumOfTheYear = new Category('Rap Album Of The Year', [ABNH, nonLikeUs])
 
-const songOfTheYearContainer = document.getElementById('song-of-the-year')
-songOfTheYear.nominations.forEach(project => {
-    const projectDiv = document.createElement('div');
-    projectDiv.className = 'project';
-    projectDiv.innerHTML = `
-        <img src="images/${project.coverArtPath}" alt="${project.name}" width="150px">
-        <h3>${project.name}</h3>
-        <p>by ${project.artiste.name}</p>
-    `;
-    songOfTheYearContainer.appendChild(projectDiv);
-});
+const categories = [
+    songOfTheYear,
+    recordOfTheYear,
+    rapAlbumOfTheYear
+]
 
-const  recordOfTheYearContainer = document.getElementById('record-of-the-year');
-recordOfTheYear.nominations.forEach(project => {
-    const projectDiv = document.createElement('div');
-    projectDiv.className = 'project';
-    projectDiv.innerHTML = `
-        <img src="images/${project.coverArtPath}" alt="${project.name}" width="150px">
-        <h3>${project.name
-        }</h3>
-        <p>by ${project.artiste.name}</p>
+const categoriesContainer = document.querySelector('.categories')
+
+categories.forEach(category => {
+    const categorySection = document.createElement('section');
+    let categoryHtml = `
+        <h2>Nominees for the ${category.name}</h2>
+        <div class="nominees">
     `;
-    recordOfTheYearContainer.appendChild(projectDiv);
+    for (let project of category.nominations) {
+        categoryHtml += `
+            <div class="project">
+                <img src="${project.coverArtPath}" alt="${project.name} cover art">
+                <span class="title">${project.name}</span>
+                <small class="artiste">${project.artiste.name}</small>
+            </div>
+        `
+    }
+    categoryHtml += '</div>'
+    categorySection.innerHTML = categoryHtml
+
+    categoriesContainer.appendChild(categorySection);
 });
